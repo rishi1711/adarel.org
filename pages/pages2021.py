c38a7f68e55950aa05c58fb554da5ab09ee6ac2c
JerrisonChang <jerrisonChang@gmail.com>
2021-11-11 17:41:56 -0500

    add figures

--- pages/pages2021.py
+++ pages/pages2021.py
@@ -28,15 +28,15 @@ def get_pages_obj(title: str, file_url: str, dataset_name: str) -> html.Div :
     ])
     return html_div
 
-def get_pages_obj_csv(title: str, file_url: str, dataset_name: str) -> html.Div :
-    html_div = html.Div([
+def get_pages_obj_csv(title: str, dataset_name: str, additional_WebDom: html.Div= None) -> html.Div :
+    itermediate = [
         dbc.Row([
             dbc.Col(
                 html.H3(title),
                 width = 4,
             ),
             dbc.Col( 
-                html.A(f"Download this data set ({dataset_name})", href=file_url),
+                html.A(f"Download this data set ({dataset_name})", href=f"/2021data/{dataset_name}"),
                 width = 4,
             ),
         ], justify='between',
@@ -48,11 +48,24 @@ def get_pages_obj_csv(title: str, file_url: str, dataset_name: str) -> html.Div 
                     dcc.Graph(figure= fgen.get_fig_from_csv(dataset_name))
                 ])
             ])
-        ])
-    ])
+        ]),
+    ]
+
+    if additional_WebDom:
+        itermediate.append(dbc.Row([dbc.Col([additional_WebDom])]))
+
+    html_div = html.Div(itermediate)
+        
     return html_div
 
-dataset_1 = get_pages_obj_csv("Empirical Study 1", "/static/2021_DataSet1.csv", "DataSet1")
-dataset_2 = get_pages_obj_csv("Empirical Study 2", "/static/2021_DataSet2.csv", "DataSet2")
-dataset_3 = get_pages_obj_csv("Empirical Study 3", "/static/2021_DataSet3.csv", "DataSet3")
-dataset_sec = get_pages_obj_csv("Empirical Study SEC", "/static/2021_DataSetSEC.csv", "DataSetSEC")
+def get_MAE_dist_fig(dataset_name: str) -> html.Div:
+    result = html.Div([
+        html.Img(src=f'/2021data/mae_dist_fig/{dataset_name}', width='800px')
+    ])
+
+    return result
+
+dataset_1 = get_pages_obj_csv("Empirical Study 1", "DataSet1", get_MAE_dist_fig("DataSet1"))
+dataset_2 = get_pages_obj_csv("Empirical Study 2", "DataSet2", get_MAE_dist_fig("DataSet2"))
+dataset_3 = get_pages_obj_csv("Empirical Study 3", "DataSet3")
+dataset_sec = get_pages_obj_csv("Empirical Study SEC", "DataSetSEC", get_MAE_dist_fig("DataSetSEC"))
