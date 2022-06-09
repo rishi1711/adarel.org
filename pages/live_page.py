d717712e3b8102fb81c00f71fde201359b0d32dd
Ninad Chaudhari <nchaudhari@albany.edu>
2022-06-09 17:44:09 -0400

    tz update on live

--- pages/live_page.py
+++ pages/live_page.py
@@ -23,8 +23,8 @@ def gen_plot_forecast():
     es_conn = fetchData.elasticSearch(url="https://kibanaadmin:kibana@kf6-stage.ikit.org/es/_search")
     df = es_conn.get_nginx_reliability(interval='1h')
     df = df.sort_values('buckets', ascending=True)
-    data_in_window = df.tail(1000)
-    data_in_window['buckets'] = data_in_window['buckets'].apply(lambda x: str(dateutil.parser.isoparse(x).astimezone(timezone('EST'))))
+    data_in_window = df.tail(100)
+    data_in_window['buckets'] = data_in_window['buckets'].apply(lambda x: str(dateutil.parser.isoparse(x).astimezone(timezone('America/New_York'))))
     rel_data = data_in_window["reliability"].to_numpy()
     date_data = data_in_window["buckets"].to_numpy()
     last_bucket = parse(data_in_window["buckets"].iloc[-1])
@@ -127,8 +127,8 @@ def update_metrics_graph(n):
               [Input('interval-component', 'n_intervals')])
 def update_metrics_prediction_div(n):
     fig, predicted_data, last_bucket,tm = gen_plot_forecast()
-    last_bucket_local = last_bucket.astimezone(timezone('EST'))
-    tm_local = tm.astimezone(timezone('EST'))
+    last_bucket_local = last_bucket.astimezone(timezone('America/New_York'))
+    tm_local = tm.astimezone(timezone('America/New_York'))
     next_bucket_time = last_bucket_local + datetime.timedelta(hours=1)
     return [
 
