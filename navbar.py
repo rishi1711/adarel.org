86b336b17d5c80bed4d3a42b37a0e0e8bab7fc78
lokhandwalapooja <plokhandwala@albany.edu>
2022-06-10 14:46:54 -0400

    requirement specifications updated

--- navbar.py
+++ navbar.py
@@ -2,7 +2,7 @@ import dash_bootstrap_components as dbc
 
 navbar = dbc.NavbarSimple(
     children=[
-        dbc.NavItem(dbc.NavLink("Sign In", href="/signin")),
+        dbc.NavItem(dbc.NavLink("Sign In", href="/login")),
         dbc.NavItem(dbc.NavLink("Sign Up", href="/signup")),
 
     ],
